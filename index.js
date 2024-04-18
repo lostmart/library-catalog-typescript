ffe0f81f16ad2f126771e47eba597e472097c077
lostmart <martinpedraza1979@gmail.com>
2024-04-18 10:27:31 +0200

    type checks, and unions

---
+++ index.js
@@ -0,0 +1,14 @@
+// types check: string, number, boolean, null, any
+// inferece (adivinar)
+var firstName = "bob";
+var age = 45;
+var admin = true;
+var laCosa = null;
+// explicit declaration:
+var bookName = "el libro";
+var date = 1765;
+// Union Type
+var userName;
+userName = "Bob";
+// userName = ""
+console.log(userName.toUpperCase());
--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("hello")
+// types check: string, number, boolean, null, any
+// inferece (adivinar)
+
+let firstName = "bob"
+
+let age = 45
+
+let admin = true
+
+let laCosa = null
+
+// explicit declaration:
+
+let bookName: string = "el libro"
+
+let date: number = 1765
+
+// Union Type
+
+let userName: string | null
+userName = "Bob"
+
+// userName = ""
+
+console.log(userName.toUpperCase())
