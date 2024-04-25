17bff378243b8f327d541dd0b0f757e786ead200
lostmart <martinpedraza1979@gmail.com>
2024-04-25 16:24:00 +0200

    new changes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TestFc from "./TestFC"
+
+function App() {
+	return (
+		<>
+			<TestFc />
+		</>
+	)
+}
+
+export default App
---
+++ src/TestFC.ts
@@ -0,0 +1,7 @@
+import React from "react"
+
+const TestFC = () => {
+	return <div>este es el test</div>
+}
+
+export default TestFC
